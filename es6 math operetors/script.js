c86eb985e11057135a6917583a76f0ed76209408
gulfamkhan2308 <sohailahmadkhan2308@gmail.com>
2023-10-19 19:45:30 -0700

    input

---
+++ es6 math operetors/script.js
@@ -0,0 +1,5 @@
+// let myNum = 5 + 3;
+// console.log(myNum + 2);
+  let myNum = 34
+  let result = 21 + myNum / 2 + 47 * 3;
+  console.log(result)
---
+++ java script helloword/script.mjs
@@ -0,0 +1,11 @@
+console.log("hello world from mjs file")
+
+let myNumber = 5;
+let myName = "Sohail";
+console.log (myNumber);
+console.log (myName);
+
+
+
+
+
